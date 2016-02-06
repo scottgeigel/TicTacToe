e8c422f80aa9c56a94240f72699c67044863851d
scott.geigel@gmail.com <scott.geigel@gmail.com>
2016-02-06 13:29:28 -0600

    Making depth entropy to minmax saving off for a refactor of the ai module to reduce parameter passing and complexity

--- src/tic_tac_toe/ai.rs
+++ src/tic_tac_toe/ai.rs
@@ -1,19 +1,20 @@
 use std::io;
 use rand;
-
 use super::board;
-fn score(game : &super::super::Game, ai_player : super::super::PlayerNumber, n : super::board::GameResult) -> i8 {
+use super::super::Player;
+
+fn score(game : &super::super::Game, ai_player : super::super::PlayerNumber, n : super::board::GameResult, depth : i8) -> i8 {
     match n {
         super::board::GameResult::XWin => {
             match ai_player {
-                super::super::PlayerNumber::PlayerX => 1,
-                super::super::PlayerNumber::PlayerO => -1,
+                super::super::PlayerNumber::PlayerX => 10 - depth,
+                super::super::PlayerNumber::PlayerO => depth - 10,
             }
         },
         super::board::GameResult::OWin => {
             match ai_player {
-                super::super::PlayerNumber::PlayerO => 1,
-                super::super::PlayerNumber::PlayerX => -1,
+                super::super::PlayerNumber::PlayerO => 10 - depth,
+                super::super::PlayerNumber::PlayerX => depth - 10,
             }
         },
         super::board::GameResult::Draw => {
@@ -22,12 +23,13 @@ fn score(game : &super::super::Game, ai_player : super::super::PlayerNumber, n :
     }
 }
 
-fn minmax(game : &super::super::Game, ai_player : super::super::PlayerNumber, active_turn : bool) -> (i8, usize) {
+fn minmax(game : &super::super::Game, ai_player : super::super::PlayerNumber, active_turn : bool, mut depth : i8) -> (i8, usize) {
     //if game over, return the score
+    depth += 1;
     match game.board.eval() {
         Some(n) => {
             let winner = n;
-            return (score(game, ai_player, winner), 10);
+            return (score(game, ai_player, winner, depth), 10);
         },
         None => {
             let mut moves = game.board.get_iterator();
@@ -37,6 +39,18 @@ fn minmax(game : &super::super::Game, ai_player : super::super::PlayerNumber, ac
             let mut smallest = 1;
             let mut greatest_choice = 1234;
             let mut smallest_choice = 5678;
+            let player : &Player;
+
+            if game.player_1.player_number == ai_player {
+                player = &game.player_1;
+            }
+            else if game.player_2.player_number == ai_player {
+                player = &game.player_2;
+            }
+            else {
+                panic!();
+            }
+
             for m in moves {
                 let mut temp_game = game.clone();
                 let current_player = temp_game.current_player.clone();
@@ -53,7 +67,7 @@ fn minmax(game : &super::super::Game, ai_player : super::super::PlayerNumber, ac
                     }
                 }
                 temp_game.next_turn();
-                let(result, choice) = minmax(&temp_game, ai_player, !active_turn);
+                let(result, choice) = minmax(&temp_game, ai_player, !active_turn, depth);
                 if result < smallest {
                     smallest_choice = m;
                     smallest = result;
@@ -81,10 +95,16 @@ fn minmax(game : &super::super::Game, ai_player : super::super::PlayerNumber, ac
                 if greatest_choices.len() > 1 {
                     return (greatest, greatest_choices[rand::random::<usize>() % greatest_choices.len()]);
                 }
+                assert!(greatest != 1234);
                 return (greatest, greatest_choice);
             }
             else {
                 //minimize
+                //mix it up a bit
+                if smallest_choices.len() > 1 {
+                    return (smallest, smallest_choices[rand::random::<usize>() % smallest_choices.len()]);
+                }
+                assert!(smallest != 5678);
                 return (smallest, smallest_choice);
             }
         },
@@ -93,6 +113,6 @@ fn minmax(game : &super::super::Game, ai_player : super::super::PlayerNumber, ac
 }
 
 pub fn decide_move(game : &mut super::super::Game) -> usize  {
-    let (result, choice) = minmax(game, game.current_player, true);
+    let (result, choice) = minmax(game, game.current_player, true, 0);
     return choice;
 }
